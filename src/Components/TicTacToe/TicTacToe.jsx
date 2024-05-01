c3fdb0b06ed3f1e2c0e127f1f359639564dc2be0
arslanrajpot <arslanarshad2307@gmail.com>
2024-05-01 17:00:04 +0500

    Tic Tac final version

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,123 @@
+import React, { useRef, useState } from 'react'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png';
+let data = ["","","","","","","","",""]
+const TicTacToe = () => {
+    let [count, setCount] = useState(0);
+    let [lock, setLock] =useState(false);
+    let titleRef = useRef(null);
+    let next_player = useRef("X");
+
+
+    let box1 = useRef(null);
+    let box2 = useRef(null);
+    let box3 = useRef(null);
+    let box4 = useRef(null);
+    let box5 = useRef(null);
+    let box6 = useRef(null);
+    let box7 = useRef(null);
+    let box8 = useRef(null);
+    let box9 = useRef(null);
+
+    let box_array=[box1,box2,box3,box4,box5,box6,box7,box8,box9]
+
+    const toggle =(e,num) =>{
+        if(lock){
+            return 0;
+        }
+
+        if(count%2===0){
+            next_player.current.innerHTML="Next Player is: O"
+            e.target.innerHTML=`<img src='${cross_icon}'>`;
+            data[num] ='x';
+            setCount(++count)
+        }
+        else{
+            next_player.current.innerHTML=" Next Player is: X"
+            e.target.innerHTML=`<img src='${circle_icon}'>`;
+            data[num] ='o';
+            setCount(++count)
+        }
+        checkWinner();
+    
+    }
+
+    const checkWinner = () => {
+        const lines = [
+          [0, 1, 2], // Horizontal lines
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6], // Vertical lines
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8], // Diagonal lines
+          [2, 4, 6]
+        ];
+      
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (data[a] && data[a] === data[b] && data[a] === data[c]) {
+            won(data[a]); 
+          }
+        }
+        return null;
+      };
+
+    const won = (winner)=>{
+        setLock(true);
+        if(winner ==="x"){
+            titleRef.current.innerHTML=`Congratulations: <img src='${cross_icon}'> Won`
+            next_player.current.innerHTML=""
+
+        }
+        else if(winner ==="o"){
+            titleRef.current.innerHTML=`Congratulations: <img src='${circle_icon}'> Won `
+            next_player.current.innerHTML=""
+
+        }
+        else{
+            titleRef.current.innerHTML="Match Tie"
+
+        }
+    } 
+
+    const reset =()=>{
+        setLock(false);
+        data = ["","","","","","","","",""]
+        titleRef.current.innerHTML = 'Tic Tac Toe Game';
+        box_array.map((e)=>{
+            e.current.innerHTML = "";
+        })
+    }
+
+
+
+
+  return (
+    <div className='container'>
+        <h1 className="title" ref={titleRef}>Tic Tac Toe Game</h1>
+        <h1 className="title" ref={next_player}> Next Player is: X </h1>
+        <div className="board">
+            <div className="row1">
+                <div className="boxes" ref={box1} onClick={(e)=>{toggle(e,0)}}></div>
+                <div className="boxes" ref={box2} onClick={(e)=>{toggle(e,1)}}></div>
+                <div className="boxes" ref={box3} onClick={(e)=>{toggle(e,2)}}></div>
+            </div>
+            <div className="row2">
+                <div className="boxes" ref={box4} onClick={(e)=>{toggle(e,3)}}></div>
+                <div className="boxes" ref={box5} onClick={(e)=>{toggle(e,4)}}></div>
+                <div className="boxes" ref={box6} onClick={(e)=>{toggle(e,5)}}></div>
+            </div>
+            <div className="row3">
+                <div className="boxes" ref={box7} onClick={(e)=>{toggle(e,6)}}></div>
+                <div className="boxes" ref={box8} onClick={(e)=>{toggle(e,7)}}></div>
+                <div className="boxes" ref={box9} onClick={(e)=>{toggle(e,8)}}></div>
+            </div>
+
+        </div>
+        <button className="reset" onClick={()=>{reset()}}>reset</button>
+    </div>
+  )
+}
+
+export default TicTacToe
